dcc97e239b1a32a329ea460266d9edb2633c5894
devGabes08 <dev.gabriel08@gmail.com>
2026-04-16 11:06:45 -0300

    Implementa a logica da funcao de anagrama

--- Code review.py
+++ Code review.py
@@ -1,5 +1,11 @@
 def sao_anagramas(string1, string2):
     # Todos: Implementar a lógica  
+    s1 = string1.replace(" ", "").lower()
+    s2 = string2.replace(" ", "").lower()
+    
+    
+    return sorted(s1) == sorted(s2) 
+    
     pass
 
 def cifra_de_cesar(texto, deslocamento):
