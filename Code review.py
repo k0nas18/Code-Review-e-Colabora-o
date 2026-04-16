b40e7064da404e2d8ff1897ff1aecdf72b29d081
KAUA KAISER RIBEIRO <kaua.kaiser@pucpr.edu.br>
2026-04-16 10:17:10 -0300

    setup inicial do projeto

---
+++ Code review.py
@@ -0,0 +1,11 @@
+def sao_anagramas(string1, string2):
+    # Todos: Implementar a lógica  
+    pass
+
+def cifra_de_cesar(texto, deslocamento):
+    # Todos: Implementar a lógica
+    pass
+
+def valida_cpf(cpf_string):
+    # Todos: Implementar a lógica
+    pass
